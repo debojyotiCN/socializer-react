aa101e5d73964343d96e835947d872a5849c432e
debojyotiCN <debojyoti.saha@capitalnumbers.com>
2021-06-07 12:18:52 +0530

    Completed all joiner form validations

--- src/components/join-room/join-room.jsx
+++ src/components/join-room/join-room.jsx
@@ -1,22 +1,154 @@
 import React, { Component } from "react";
 
 class JoinRoom extends Component {
-  state = {};
+  state = {
+    formFields: {
+      roomId: {
+        value: "",
+        isValid: false,
+        isDirty: false,
+      },
+      userName: {
+        value: "",
+        isValid: false,
+        isDirty: false,
+      },
+    },
+    isFormValid: false,
+    redirectTo: null,
+    errorText: "",
+    isLoading: false,
+  };
+
+  _markAsDirty = (fieldName) => {
+    const { formFields } = this.state;
+    formFields[fieldName].isDirty = true;
+    this.setState({ formFields }, () => {
+      this._validateForm();
+    });
+  };
+
+  _toggleLoader = (isLoading) => {
+    this.setState({ isLoading });
+  };
+
+  _showError = (errorText) => {
+    this.setState({ errorText });
+  };
+
+  _hideError = () => {
+    this.setState({ errorText: "" });
+  };
+
+  _updateFieldValue = (fieldName, value) => {
+    const { formFields } = this.state;
+    formFields[fieldName].value = value;
+    this.setState({ formFields });
+    if (formFields[fieldName].isDirty) {
+      // Validate
+      this._validateForm();
+    }
+  };
+
+  _validateForm = () => {
+    return new Promise((resolve, reject) => {
+      const { formFields } = this.state;
+      let isFormValid = true;
+      Object.keys(formFields).forEach((fieldName, index) => {
+        if (isFormValid && formFields[fieldName].isDirty) {
+          switch (fieldName) {
+            case "userName": {
+              if (formFields.userName.value.length >= 1) {
+                formFields.userName.isValid = true;
+                this._hideError();
+              } else {
+                formFields.userName.isValid = false;
+                isFormValid = false;
+                this._showError("Please enter a valid User Name");
+              }
+              break;
+            }
+            case "roomId": {
+              if (formFields.roomId.value.length >= 1) {
+                this._hideError();
+                formFields.roomId.isValid = true;
+              } else {
+                formFields.roomId.isValid = false;
+                isFormValid = false;
+                this._showError("Please enter a valid Room Id");
+              }
+              break;
+            }
+          }
+        }
+      });
+      this.setState({ formFields, isFormValid }, () => {
+        resolve();
+      });
+    });
+  };
+
+  _makeAllFieldDirty = () => {
+    return new Promise((resolve, reject) => {
+      const { formFields } = this.state;
+      Object.keys(formFields).forEach((fieldName, index) => {
+        formFields[fieldName].isDirty = true;
+      });
+      this.setState({ formFields }, () => {
+        resolve();
+      });
+    });
+  };
+
+  _validateAndSubmit = async (e) => {
+    e.preventDefault();
+    const { formFields, isFormValid, redirectTo } = this.state;
+    await this._makeAllFieldDirty();
+    await this._validateForm();
+    if (isFormValid) {
+      try {
+        this._toggleLoader(true);
+      } catch (loginError) {
+        this._toggleLoader(false);
+        this._showError("Login server error");
+      }
+    }
+  };
+
   render() {
+    const { errorText, formFields, isLoading } = this.state;
+
     return (
-      <div className="tabContent">
+      <form className="tabContent" onSubmit={this._validateAndSubmit}>
         <div className="inputWrapper">
           <div className="label">Room Id</div>
-          <input type="text" />
+          <input
+            type="text"
+            value={formFields.roomId.value}
+            disabled={isLoading}
+            onChange={(e) => this._updateFieldValue("roomId", e.target.value)}
+            onBlur={() => this._markAsDirty("roomId")}
+          />
         </div>
         <div className="inputWrapper">
           <div className="label">User Name</div>
-          <input type="text" />
+          <input
+            type="text"
+            value={formFields.userName.value}
+            disabled={isLoading}
+            onChange={(e) => this._updateFieldValue("userName", e.target.value)}
+            onBlur={() => this._markAsDirty("userName")}
+          />
         </div>
-        <button className="submitBtn">Join</button>
-        {/* <div className="progressViewer">Please wait</div> */}
-        <p className="errorText">Something wrong happended</p>
-      </div>
+        {isLoading ? (
+          <div className="progressViewer">Please wait</div>
+        ) : (
+          <button className="submitBtn" type="submit">
+            Join
+          </button>
+        )}
+        <p className="errorText">{errorText}</p>
+      </form>
     );
   }
 }
